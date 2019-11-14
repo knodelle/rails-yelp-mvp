43732c95a2550c0d5c14985a510ec09a243fadfa
Marie Begue <marie.prandstetter@gmail.com>
2019-11-14 11:42:01 +0100

    to be continued

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -1,2 +1,20 @@
 class RestaurantController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    # Unless @restaurant.valid?, #save will return false,
+    # and @restaurant is not persisted.
+    # TODO: present the form again with error messages.
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :stars)
+  end
 end
